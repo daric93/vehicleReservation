09bf3d3ba9810c2a10252f29602ef003d0899830
Darya Koreneva <daric2612@gmail.com>
2016-03-26 17:43:34 +0200

    add method getUserByMail

--- src/main/java/daric/vr/services/UserService.java
+++ src/main/java/daric/vr/services/UserService.java
@@ -4,7 +4,7 @@ import daric.vr.entities.User;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -31,7 +31,17 @@ public class UserService {
     @GET
     @Path("{id}")
     public User getUser(@PathParam("id") int id) {
-            return em.find(User.class, id);
+        return em.find(User.class, id);
+    }
+
+    @GET
+    @Path("{username}")
+    public User getUserByMail(@PathParam("mail") String mail) {
+        try {
+            return (User) em.createNamedQuery("getUserByMail").setParameter("mail", mail).getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 
     @PUT

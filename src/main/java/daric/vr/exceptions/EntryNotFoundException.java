f0b8d218e314d8c07dda48c531724b44e501a6d4
Darya Koreneva <daric2612@gmail.com>
2016-05-26 19:33:03 +0300

    added ApplicationException annotation

--- src/main/java/daric/vr/exceptions/EntryNotFoundException.java
+++ src/main/java/daric/vr/exceptions/EntryNotFoundException.java
@@ -1,9 +1,11 @@
 package daric.vr.exceptions;
 
+import javax.ejb.ApplicationException;
 import javax.ws.rs.NotFoundException;
 
 import static daric.vr.exceptions.ExceptionUtil.getResponseWithMessage;
 
+@ApplicationException(rollback = true)
 public class EntryNotFoundException extends NotFoundException {
     public EntryNotFoundException(String message) {
         super(message, getResponseWithMessage(message, 404));
--- src/main/java/daric/vr/exceptions/RequiredFieldIsMissingException.java
+++ src/main/java/daric/vr/exceptions/RequiredFieldIsMissingException.java
@@ -1,9 +1,11 @@
 package daric.vr.exceptions;
 
+import javax.ejb.ApplicationException;
 import javax.ws.rs.ClientErrorException;
 
 import static daric.vr.exceptions.ExceptionUtil.getResponseWithMessage;
 
+@ApplicationException(rollback = true)
 public class RequiredFieldIsMissingException extends ClientErrorException {
     public RequiredFieldIsMissingException(String message) {
         super(message, getResponseWithMessage(message, 400));

fe50ef2a2d93032a06f86ef3ba2a544a88e8a0cf
Darya Koreneva <daric2612@gmail.com>
2016-05-26 13:22:41 +0300

    changed to implement Filter

--- src/main/java/daric/vr/servlets/GetCarTypes.java
+++ src/main/java/daric/vr/servlets/GetCarTypes.java
@@ -4,28 +4,31 @@ import daric.vr.entities.CarType;
 import daric.vr.services.CarTypeService;
 
 import javax.ejb.EJB;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-public class GetCarTypes extends HttpServlet {
+public class GetCarTypes implements Filter {
     @EJB
     CarTypeService carTypeService;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<CarType> carTypes = carTypeService.getAll();
-        req.setAttribute("carTypes", carTypes);
-        RequestDispatcher dispatcher = req.getRequestDispatcher("carTypes.jsp");
-        dispatcher.forward(req,resp);
+    public void init(FilterConfig filterConfig) throws ServletException {
+
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req,resp);
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        List<CarType> carTypes = carTypeService.getAll();
+        request.setAttribute("carTypes", carTypes);
+        chain.doFilter(request,response);
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }

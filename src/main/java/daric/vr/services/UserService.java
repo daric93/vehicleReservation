d7c8902ee68069bcec00da77774c34dc11cd92df
Darya Koreneva <daric2612@gmail.com>
2016-05-26 16:40:01 +0300

    removed method update, added checks for duplicate entry, missing fields, not existing entry

--- src/main/java/daric/vr/services/UserService.java
+++ src/main/java/daric/vr/services/UserService.java
@@ -1,14 +1,15 @@
 package daric.vr.services;
 
+import com.google.common.base.Throwables;
 import daric.vr.entities.User;
+import daric.vr.exceptions.DuplicateEntryException;
+import daric.vr.exceptions.RequiredFieldIsMissingException;
 
 import javax.ejb.Stateless;
-import javax.persistence.EntityGraph;
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
+import javax.persistence.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.Optional;
 
 @Stateless
 @Path("userService")
@@ -20,38 +21,47 @@ public class UserService {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public User addUser(User user) {
-        return em.merge(user);
+        try {
+            User user1 = em.merge(user);
+            em.flush();
+            return user1;
+        } catch (PersistenceException e) {
+            if (Throwables.getCausalChain(e).stream().anyMatch(ex -> ex.getMessage().contains("Duplicate entry"))) {
+                throw new DuplicateEntryException("User with this mail already exists");
+            } else {
+                Optional<Throwable> opt = Throwables.getCausalChain(e).stream().filter(ex -> ex.getMessage().contains("cannot be null")).findAny();
+                if (opt.isPresent())
+                    throw new RequiredFieldIsMissingException(opt.get().getMessage());
+                else throw e;
+            }
+        }
     }
 
     @DELETE
     @Path("{id}")
     public void deleteUser(@PathParam("id") int id) {
-        em.remove(em.find(User.class, id));
+        em.remove(getUser(id));
     }
 
     @GET
     @Path("{id}")
     public User getUser(@PathParam("id") int id) {
-        return em.find(User.class, id);
+        User user = em.find(User.class, id);
+        if (user == null)
+            throw new NotFoundException("User with this id is not found");
+        return user;
     }
 
     @GET
-    @Path("{username}")
+    @Path("{mail}")
     public User getUserByMail(@PathParam("mail") String mail) {
         EntityGraph graph = em.createEntityGraph("graph.User.getOrders");
-
         try {
             return (User) em.createNamedQuery("getUserByMail").
                     setParameter("mail", mail).setHint("javax.persistence.fetchgraph", graph).
                     getSingleResult();
         } catch (NoResultException e) {
-            return null;
+            throw new NotFoundException(e.getMessage());
         }
-    }
-
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public User updateUser(User user) {
-        return em.merge(user);
     }
 }

9ee744872219ff7c2f67fd40a6cd7b34d37ed1b7
Darya Koreneva <daric2612@gmail.com>
2016-05-26 13:25:54 +0300

    servlet for getting CarType

---
+++ src/main/java/daric/vr/servlets/GetCarType.java
@@ -0,0 +1,25 @@
+package daric.vr.servlets;
+
+import daric.vr.entities.CarType;
+import daric.vr.services.CarTypeService;
+import sun.rmi.server.Dispatcher;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GetCarType extends HttpServlet{
+    @EJB
+    CarTypeService carTypeService;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CarType carType = carTypeService.getCarType(Integer.parseInt(req.getParameter("carTypeId")));
+        req.setAttribute("carType",carType);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("editCarType.jsp");
+        dispatcher.forward(req,resp);
+    }
+}
